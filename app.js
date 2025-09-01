cd46aa0a273a58a44d0607350144b1e3e660d7c7
tuliotechh <marcotuliosante409@gmail.com>
2025-09-01 12:34:38 -0300

    codigo completo

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+    let nomeAmigo = document.querySelector("input").value;
+    if (nomeAmigo == "") {
+        alert("Por favor, insira um nome");
+    } else {
+        amigos.push(nomeAmigo);
+        console.log(amigos);
+        document.querySelector("input").value = "";
+    }
+}
+
+function atualizarLista() {
+    let lista = document.querySelector("#listaAmigos");
+    lista.innerHTML = "";
+}
+
+for (let i = 0; i < amigos.length; i++) {
+    let itemListaAmigos = document.createElement("li");
+    item.textContent = amigos[i];
+    lista.appendChild(itemListaAmigos);
+}
+
+function sortearAmigo() {
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    let amigoSorteado = amigos[indiceAleatorio];
+    document.querySelector("#resultado").innerHTML = "Amigo sorteado: " + amigoSorteado;
+}
+
